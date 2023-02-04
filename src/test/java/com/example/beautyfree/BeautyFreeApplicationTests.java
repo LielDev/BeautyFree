d8f99175860c0b49f504a5dd06f818dedb394d00
LielDev <117684012+LielDev@users.noreply.github.com>
2023-02-04 22:39:04 +0100

    Creazione del progetto

---
+++ src/test/java/com/example/beautyfree/BeautyFreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beautyfree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyFreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
